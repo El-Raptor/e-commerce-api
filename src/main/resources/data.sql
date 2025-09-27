2db2957f5e4747e93e96f95a83157ca3fd8b5ef8
Felipe Salles Lopes <felipe28101997@gmail.com>
2025-09-27 12:07:42 -0400

    chore(db): configure H2 database and seed initial product data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO product (name, description, brand, price, category, release_date, available, quantity)
+VALUES ( 'Iphone 15', 'Smartphone 7"', 'Apple', 4650.0, 'Smartphone','2022-09-22', true, 100 );
